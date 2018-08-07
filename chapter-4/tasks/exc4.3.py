4d22ffd3e1b69091e76f41d28a95055eaf6bbcf9
vogdb <vozhdb@gmail.com>
2018-08-07 17:59:54 +0300

    task 4.3

---
+++ chapter-4/tasks/exc4.3.py
@@ -0,0 +1,17 @@
+from util import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+p_mtrx = np.random.choice([0, 1], size=(4,20), p=[0.9, 0.1])
+print(p_mtrx)
+hb, pr, po, hp = build_connectivity_matrix(p_mtrx)
+
+fig = plt.figure()
+
+for idx, p in enumerate(p_mtrx):
+    state = async_update(hb, p, 100)
+    # state = async_update(hp, p, 100)
+    ax_s = fig.add_subplot(23 * 10 + idx + 1)
+    ax_s.imshow(state, cmap='gray')
+
+plt.show()
