bbaab5da5ccd11f41223992cf8949a0fcc0266f5
vogdb <vozhdb@gmail.com>
2018-08-13 10:50:06 +0300

    task 5.4

---
+++ chapter-5/tasks/exc5.4.py
@@ -0,0 +1,41 @@
+import numpy as np
+from util import *
+np.set_printoptions(precision=2)
+
+pattern_list = []
+pattern_list.append(np.array([
+    [1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1],
+]))
+pattern_list.append(np.array([
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+]))
+pattern_list.append(np.array([
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+]))
+pattern_list.append(np.array([
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+]))
+
+input = np.array(pattern_list)
+shape = input.shape
+input = input.reshape((shape[0], shape[1] * shape[2]))
+print(input)
+
+output = kohonen_som(input, n_out=7, n_hood=1, n_its=100)
+print(output)
