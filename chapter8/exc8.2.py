4e19196fd81a321f253e950bfa2cb9e88d552627
vogdb <vozhdb@gmail.com>
2018-08-30 19:41:06 +0300

    task 8.2

--- chapter8/exc8.2.py
+++ chapter8/exc8.2.py
@@ -1,64 +1,64 @@
 import numpy as np
-from infocomp import infocomp
+import matplotlib.pyplot as plt
 
-px1, px2 = .5, .5
-train_num = 3000
+train_num = 20000
 lr = .01
-threshold = .51
-x_train = np.zeros((2, train_num))
-x_train[0, :] = np.random.rand(train_num) < px1
-x_train[1, :] = np.random.rand(train_num) < px2
-input = np.array([
-    [0, 0],
-    [1, 0],
-    [0, 1],
-    [1, 1],
-])
-V = np.random.rand(2, 2)
-b = np.random.rand(2, 1)
 
+f = .5
+t = np.linspace(0, (1 / f) * 10, train_num)
+x_original = np.zeros((2, train_num))
+x_original[0, :] = .5 * np.sin(2 * np.pi * f * t)
+x_original[1, :] = np.random.rand(train_num) - .5
+mix = np.array([
+    [.6, .4],
+    [.5, .5],
+])
+x_train = mix.dot(x_original)
+
+V = np.array([
+    [.03, .02],
+    [.01, .04],
+])
+b = np.array([
+    [.02],
+    [.03],
+])
+
+batch_size = 50
+del_V = np.zeros(V.shape)
+del_b = np.zeros(b.shape)
 for c in range(train_num):
     x = x_train[:, c][:, np.newaxis]
     q = V.dot(x) + b
     y = 1. / (1 + np.exp(-q))
-    del_V = lr * (np.linalg.inv(V).T + (1 - 2 * y).dot(x.T))
-    del_b = lr * (1 - 2 * y)
-    V += del_V
-    V[V < 0] = 0
-    b += del_b
+    if c % batch_size == 0:
+        V += del_V
+        V[V < 0] = 0
+        b += del_b
+        del_V = np.zeros(V.shape)
+        del_b = np.zeros(b.shape)
+    else:
+        del_V += lr * (np.linalg.inv(V).T + (1 - 2 * y).dot(x.T))
+        del_b += lr * (1 - 2 * y)
 
-output = np.zeros(input.shape)
-in_state_num = input.shape[0]
-out_state_num = output.shape[0]
-condi = np.zeros((out_state_num, in_state_num))
-
-for l in range(in_state_num):
-    x = input[l][:, np.newaxis]
+y = np.empty_like(x_train)
+for c in range(train_num):
+    x = x_train[:, c][:, np.newaxis]
     q = V.dot(x) + b
-    y = 1 / (1 + np.exp(-q))
-    y = y > threshold
-    y = np.asarray(y, dtype=int).flatten()
-    if np.array_equal(y, [0, 0]):
-        condi[0, l] = 1
-    elif np.array_equal(y, [1, 0]):
-        condi[1, l] = 1
-    elif np.array_equal(y, [0, 1]):
-        condi[2, l] = 1
-    elif np.array_equal(y, [1, 1]):
-        condi[3, l] = 1
-    output[l] = y
+    y[:, c] = (1. / (1 + np.exp(-q))).flatten()
 
-pX = np.zeros(in_state_num)
-pX[0] = (1 - px1) * (1 - px2)
-pX[1] = px1 * (1 - px2)
-pX[2] = (1 - px1) * px2
-pX[3] = px1 * px2
+ax_original1 = plt.subplot(221)
+ax_original1.set_title('original')
+ax_original1.plot(x_original[0])
+ax_original2 = plt.subplot(222)
+ax_original2.set_title('original')
+ax_original2.plot(x_original[1])
 
-hX, hY, I = infocomp(pX, condi)
+ax_restored1 = plt.subplot(223)
+ax_restored1.set_title('restored')
+ax_restored1.plot(y[0])
+ax_restored2 = plt.subplot(224)
+ax_restored2.set_title('restored')
+ax_restored2.plot(y[1])
 
-print(input)
-print(output)
-print(V)
-print(condi)
-
-print('H_X: {:.2f}, H_Y: {:.2f}, I: {:.2f}'.format(hX, hY, I))
+plt.savefig('exc8.2.svg')
