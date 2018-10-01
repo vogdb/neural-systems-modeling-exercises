872bdcbcf0598a1e5ca78c9d47dddb6ffc1d8afe
vogdb <vozhdb@gmail.com>
2018-10-01 18:00:50 +0300

    11 midbrain dopamine lms

--- chapter11/midbrain_dopamine_lms.py
+++ chapter11/midbrain_dopamine_lms.py
@@ -2,8 +2,7 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-lr = 0.3
-n_its = 200
+n_its = 20
 n_time = 50
 
 x = np.zeros(n_time)
@@ -20,28 +19,26 @@ q_time = 10
 r_time = 30
 x[q_time:r_time] = 1
 
+# rtg = 1
+# r[r_time] = 1
+
 for c in range(n_its):
-    # default, when the signal is built up
-    # drop_r = int(n_its / 2)
-    # during raise of the signal
-    # drop_r = int(n_its / 10)
-    # if c == drop_r:
-    #     r[r_time] = 0
-    # else:
-    #     r[r_time] = 1
-    # jumping reward signal
-    r_jitter = r_time - np.random.randint(4)
-    r[r_jitter] = 1
+    if c == int(n_its / 2):
+        rtg = 0
+        r[r_time] = 0
+    else:
+        rtg = 1
+        r[r_time] = 1
     y = np.concatenate(([0], np.diff(v * x)))
     z = y + r
-    v = v + lr * x * np.concatenate((z[1:], [0]))
+    v = v + (rtg - v) / (c + 1)
     t_course[c, :] = z
 
 X = np.arange(0, n_time)
 Y = np.arange(0, n_its)
 X, Y = np.meshgrid(X, Y)
 
-fig = plt.figure(figsize=(14,10))
+fig = plt.figure(figsize=(14, 10))
 ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(X, Y, t_course, cmap='plasma')
 ax.set_xlabel('time')
